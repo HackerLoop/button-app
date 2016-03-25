af56a94446d007c4a514d97fc7eb22a44af6bf61
squirelo <valentin.squirelo@gmail.com>
2016-03-25 20:06:00 +0100

    add global shortcuts handling, get button pressed event <3

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-var menubar = require('menubar')
+var globalShortcut = require('global-shortcut')
 
+var menubar = require('menubar')
 var mb = menubar()
+
 
 mb.on('ready', function ready () {
   console.log('app is ready')
+  setGlobalShortcuts();
 })
+
+function setGlobalShortcuts() {
+
+
+    globalShortcut.register('ctrl+alt+shift+b', function () {
+        console.log('button is pressed')
+    });
+}
