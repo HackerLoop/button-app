cd70cda7b6f7265e92e488fc66101da658f70126
squirelo <valentin.squirelo@gmail.com>
2016-03-27 18:03:01 +0200

    add a buttonUrl var in json storage

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 var ipcMain = require('electron').ipcMain
+var configuration = require('./configuration');
 var globalShortcut = require('global-shortcut');
 var request = require('request');
 var menubar = require('menubar');
@@ -12,21 +13,27 @@ var mb = menubar({
 
 
 mb.on('ready', function ready () {
+  if (!configuration.readSettings('buttonUrl')) {
+      configuration.saveSettings('buttonUrl', 'https://maker.ifttt.com/trigger/button_pressed/with/key/mdWQxkYhM1-LuraKiqDmaXb_euu5AWqNKsPh63bTy0o');
+  }
   console.log('app is ready')
-  setGlobalShortcuts();
+  setGlobalShortcuts()
 })
 
 function setGlobalShortcuts() {
-    globalShortcut.unregisterAll();
-    globalShortcut.register('ctrl+alt+shift+y', function () {
-        pressButton()
-        console.log('button is pressed')
-    });
+  globalShortcut.unregisterAll();
+  globalShortcut.register('ctrl+alt+shift+y', function () {
+      pressButton()
+      console.log('button is pressed')
+  });
 }
 
+
 function pressButton() {
+  var buttonUrl = configuration.readSettings('buttonUrl');
+  console.log('request is sending')
   request.post(
-    'https://maker.ifttt.com/trigger/button_pressed/with/key/mdWQxkYhM1-LuraKiqDmaXb_euu5AWqNKsPh63bTy0o',
+    buttonUrl,
     {form:{event:'button_pressed'}},
     function (error, response, body) {
         if (!error && response.statusCode == 200) {
