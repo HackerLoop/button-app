149b2df3b390f82179284e6bed3e9d86f2fb282c
squirelo <valentin.squirelo@gmail.com>
2016-03-27 18:57:23 +0200

    add grid css + red button in the app

--- app/js/index.js
+++ app/js/index.js
@@ -2,7 +2,12 @@
 'use strict';
 var ipcRenderer = require('electron').ipcRenderer
 var closeEl = document.querySelector('.close');
+var saveUrl = document.querySelector('.save');
 
 closeEl.addEventListener('click', function () {
     ipcRenderer.send('close-app');
+});
+
+closeEl.addEventListener('click', function () {
+    ipcRenderer.send('save-url');
 });
